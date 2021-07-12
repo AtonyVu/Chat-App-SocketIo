a2e34613994777177d706c9b0f1c20f8fb3789c5
luancauthu2907 <luancauthu@gmail.com>
2021-07-12 23:43:29 +0700

    update

--- app/public/js/client.js
+++ app/public/js/client.js
@@ -1,5 +1,5 @@
 const socket = io();
-
+let id = "";
 const Acknewledgement = () => {
   console.log("đã gửi tin nhắn ");
 };
@@ -51,31 +51,60 @@ socket.on("server-to-client", (chat) => {
       src="https://www.bootdey.com/img/Content/avatar/avatar3.png"
       alt="Retail Admin"
     />
-    <div class="chat-name">Sam</div>
+    <div class="chat-name">${chat.user}</div>
   </div>
   </li>`;
 });
-socket.on("join-room", (welcom) => {
-  console.log(welcom);
+socket.on("join-room", (id1) => {
+  id = id1;
+  console.log(id);
 });
-socket.on("new-join-room", (newjoin) => {
-  let ul = document.getElementById("users");
-  ul.innerHTML += `  <li class="person" data-chat="person1">
-  <div class="user">
-    <img
-      src="https://www.bootdey.com/img/Content/avatar/avatar3.png"
-      alt="Retail Admin"
-    />
-    <span class="status busy"></span>
-  </div>
-  <p class="name-time">
-    <span class="name">${newjoin}</span>
-    <span class="time">${new Date()}</span>
-  </p>
-</li>`;
-  console.log(newjoin);
+socket.on("new-join-room", (userList) => {
+  console.log(userList);
+  if (userList.length != 0) {
+    let ul = document.getElementById("users");
+    ul.innerHTML = "";
+    userList.forEach((item) => {
+      if (item.id != id) {
+        ul.innerHTML += `  <li class="person" data-chat="person1">
+        <div class="user">
+          <img
+            src="https://www.bootdey.com/img/Content/avatar/avatar3.png"
+            alt="Retail Admin"
+          />
+          <span class="status busy"></span>
+        </div>
+        <p class="name-time">
+          <span class="name">${item.Name}</span>
+          <span class="time">${item.date}</span>
+        </p>
+      </li>`;
+      }
+    });
+  }
 });
 socket.emit(
   "join-info-room",
   Qs.parse(location.search, { ignoreQueryPrefix: true })
 );
+socket.on("dis-conect", (userList) => {
+  let ul = document.getElementById("users");
+  ul.innerHTML = "";
+  userList.forEach((item) => {
+    if (item.id != id) {
+      ul.innerHTML += `  <li class="person" data-chat="person1">
+      <div class="user">
+        <img
+          src="https://www.bootdey.com/img/Content/avatar/avatar3.png"
+          alt="Retail Admin"
+        />
+        <span class="status busy"></span>
+      </div>
+      <p class="name-time">
+        <span class="name">${item.Name}</span>
+        <span class="time">${item.date}</span>
+      </p>
+    </li>`;
+    }
+  });
+});
--- app/src/server.js
+++ app/src/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dateFormat = require("date-format");
 const http = require("http");
 const app = express();
 const path = require("path");
@@ -7,7 +8,7 @@ const pathPublicDirectory = path.join(__dirname, "../public");
 const socketIo = require("socket.io");
 var Filter = require("bad-words");
 var customFilter = new Filter({ placeHolder: "x" });
-
+let userList = [];
 customFilter.clean("Don't be an ash0le");
 app.use(express.static(pathPublicDirectory));
 const server = http.createServer(app);
@@ -17,28 +18,37 @@ const port = process.env.PORT || 5000;
 const io = socketIo(server);
 ///ket noi server voi client
 io.on("connection", (socket) => {
-  socket.on("disconnect", () => {
-    console.log(" client dis connect", socket.id);
-  });
   socket.on("join-info-room", ({ Room, Name }) => {
     socket.join(Room);
-    socket.emit("join-room", "Welcom " + Name);
-    socket.broadcast.to(Room).emit("new-join-room", Name);
+    userList.push({
+      Name: Name,
+      date: dateFormat("dd/MM/yyyy - hh:mm:ss", new Date()),
+      id: socket.id,
+    });
+    socket.emit("join-room", socket.id);
+    io.to(Room).emit("new-join-room", userList);
+
     socket.on("client-to-server", (chat, callback) => {
       console.log(chat.chat);
       callback();
       socket.broadcast.to(Room).emit("server-to-client", {
         ...chat,
-        user: socket.id,
+        user: Name,
         chat: customFilter.clean(chat.chat),
       });
+    });
+    socket.on("disconnect", () => {
+      let list = userList.filter((item) => {
+        return item.id != socket.id;
+      });
+
+      userList = [...list];
+      io.to(Room).emit("dis-conect", userList);
+      console.log(" client dis connect", Name);
     });
   });
 });
 
-io.on("disconnect", (socket) => {
-  console.log("out connect", socket.id);
-});
 server.listen(port, () => {
   console.log("app run on http://localhost:" + port);
 });
